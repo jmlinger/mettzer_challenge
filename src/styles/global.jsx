d9a0d25b39553225a65950f49668e6652df75b93
jmlinger <jmlinger10@gmail.com>
2022-09-09 10:03:56 -0300

    feat: add global styles.

---
+++ src/styles/global.jsx
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+body {
+  font-family: -apple-system, sans-serif;
+  margin: 0;
+  background-color: #FFFAF0;
+  padding: 0;
+  max-width: 100vw;
+  color: black;
+}
+`;
