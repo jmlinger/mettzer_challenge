f0e0bb579b5741ece3b72b7b2bb0b043648e0b27
jmlinger <jmlinger10@gmail.com>
2022-09-09 10:04:43 -0300

    feat: add table component styles.

---
+++ src/styles/pages/table.jsx
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+
+const SubHeader = styled.div`
+  align-content: center;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+
+  button {
+    background-color: #1e90ff;
+    border-radius: 15%;
+    margin-left: 0.5em;
+    margin-right: 1em;
+    &:disabled {
+      opacity: 0.6;
+    }
+  }
+`;
+
+export const SubHeaderOff = styled(SubHeader)`
+  div {
+    align-content: center;
+    align-items: center;
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+  }
+
+  button:first-child {
+    background-color: gold;
+  }
+`;
+
+export const SubHeaderOn = styled(SubHeader)`
+  div {
+    align-content: center;
+    align-items: center;
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+  }
+`;
