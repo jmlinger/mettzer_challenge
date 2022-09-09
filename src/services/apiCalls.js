bce4d21c69fbf838798066d71fd1da908ea43828
jmlinger <jmlinger10@gmail.com>
2022-09-09 09:58:38 -0300

    feat: add apicalls manager service.

---
+++ src/services/apiCalls.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+export const apiCustList = async (search, page = 1, perPage) => {
+  try {
+    const { string } = search;
+    const url = `https://core.ac.uk:443/api-v2/search/${string}?page=${page}&pageSize=${perPage}&apiKey=RxFGPhoNOJjkDAvgVX6ZU1Wa2HsQ4q53
+    `;
+
+    const fetchApi = await axios.get(url);
+    const response = await fetchApi.data;
+    console.log(search, page, perPage);
+    console.log(response);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
