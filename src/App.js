8529569c0919b91dd7a5736942d3ad218b5ff4a0
jmlinger <jmlinger10@gmail.com>
2022-09-09 09:56:18 -0300

    build and chore: add initial dependencies and linters

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Routes from './routes/Routes';
+import GlobalStyle from './styles/global';
+
+function App() {
+  return (
+    <div>
+      <GlobalStyle />
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
